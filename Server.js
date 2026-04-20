6f4e59d7a0a2099c90ff4654a9016e515dbf708f
AyushKPukreti <ayushpkukreti@gmail.com>
2026-04-20 21:43:16 +0530

    server domain configs

--- Server.js
+++ Server.js
@@ -16,7 +16,7 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: ["http://localhost:5174", "http://localhost:5173", "https://billings.thearmtechnologies.com"],
+    origin: ["http://localhost:5174", "http://localhost:5173", "https://billings.thearmtechnologies.com", "https://api.thearmtechnologies.com/"],
     methods: 'GET,POST,PUT,DELETE,OPTIONS,HEAD,PATCH',
     credentials: true,
 }
