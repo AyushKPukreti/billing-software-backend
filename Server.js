043ed9c62ec4fae8232151ccdd5e6f7042d6f4e3
AyushKPukreti <ayushpkukreti@gmail.com>
2026-04-20 22:56:20 +0530

    added proxy line

--- Server.js
+++ Server.js
@@ -13,10 +13,13 @@ import { seedAdminUser } from './utils/seedAdmin.js';
 dotenv.config()
 
 const app = express()
+
+app.set("trust proxy", 1);
+
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: ["http://localhost:5174", "http://localhost:5173", "https://billings.thearmtechnologies.com", "https://api.thearmtechnologies.com/"],
+    origin: ["http://localhost:5174", "http://localhost:5173", "https://billings.thearmtechnologies.com"],
     methods: 'GET,POST,PUT,DELETE,OPTIONS,HEAD,PATCH',
     credentials: true,
 }
